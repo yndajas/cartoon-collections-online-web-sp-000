db89dd54696500d0139ec11be70443fde8e1e896
Ynda Jas <yndajas@gmail.com>
2020-07-06 01:52:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def find_the_cheese(items)
   cheese_types = ["cheddar", "gouda", "camembert"]
   items.collect do |item|
     if cheese_types.include?(item)
-      item
+      return item
     end
   end
 end
