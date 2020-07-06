87769d16852e2231bc1f3e0853dff992c50f5cdd
Ynda Jas <yndajas@gmail.com>
2020-07-06 01:18:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf,index|
+    puts "#{index} "
 end
 
 def summon_captain_planet# code an argument here
