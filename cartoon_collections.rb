8905541a561c85c22153049f29c1980d51cee972
Ynda Jas <yndajas@gmail.com>
2020-07-06 01:46:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,10 @@ def summon_captain_planet(planeteer)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.collect do |call|
+    call.length > 4
+  end.include?(true)
 end
 
 def find_the_cheese# code an argument here
